d353442a0be58c8582cee63ed5c2b83bf32b60db
Gustave.A Ilunga <ilungagustave73@gmail.com>
2026-07-04 11:47:39 +0200

    .

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,12 +2,10 @@ import Button from "./ui/Button";
 
 export default function HeroSection() {
     return (
-        <section className="mb-12 bg-green-500">
-            <div className="relative overflow-hidden rounded-lg bg-slate-900
-  min-h-[420px] md:min-h-[520px]
-  flex items-center">
+        <section className="mb-12 ">
+            <div className="relative overflow-hidden rounded-lg bg-slate-900 min-h-105 md:min-h-130 flex items-center">
                 <div className="absolute inset-0 z-0">
-                    <div className="absolute inset-0 bg-linear-to-r from-black/70 via-black/40 to-transparent z-10"></div>
+                    <div className="absolute inset-0 bg-black/40  z-10"></div>
                     <img alt="Living Room Hero" className="w-full h-full object-cover" data-alt="Modern styled living room with minimal furniture" src="https://lh3.googleusercontent.com/aida-public/AB6AXuBG8RbtKexnPmMOJWiBPY3LNmtl8NQLd5sZkJ2h_bJoBulNIcv4iFfEJNCgdhS14Il6vHWySf6Aa2-Iq9_B2hdta1SjFfb7IHfpfVOSg1dlyWgV-R3TOWh0WOehztPTnuJ6rGybhxMFPqt1fAY7IWlOmkqtB4wPNBgzj-hL2b5M6emJOcuPAwJJ8poW5_7Jmqihp4CWUUCB2YSZNWCjKeULufpGQmld6-YWrcFzS5MDqYzKc_jbmB6HWIWJe9_DlT-3PZdpHrKfw1vC" />
                 </div>
                 <div className="relative z-20 px-12 max-w-2xl">
@@ -17,7 +15,7 @@ export default function HeroSection() {
                     <p className="text-white/90 text-lg mb-8 font-normal max-w-md">
                         Discover curated home essentials designed for comfort and style. From minimal chairs to luxury beds.
                     </p>
-                    <div className="flex gap-4 hidden">
+                    <div className="flex gap-4 ">
                         <Button>
                             Shop Now
                         </Button>
